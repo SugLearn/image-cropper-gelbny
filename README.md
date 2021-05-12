e4381a9f8442fd1e1cbfb6356c9f4e69211d7146
SugLearn <saranya.sakthivel06@gmail.com>
2021-05-12 19:36:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# image-cropper-gelbny
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/image-cropper-gelbny)
---
+++ app/app.component.ts
@@ -0,0 +1,56 @@
+import { Component, ViewChild } from '@angular/core';
+import {
+  Dimensions,
+  ImageCroppedEvent,
+  ImageTransform
+} from './image-cropper/interfaces/index';
+import { base64ToFile } from './image-cropper/utils/blob.utils';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  imageChangedEvent: any = '';
+  croppedImage: any = '';
+  canvasRotation = 0;
+  rotation = 0;
+  scale = 1;
+  showCropper = false;
+  containWithinAspectRatio = false;
+  transform: ImageTransform = {};
+
+  fileChangeEvent(event: any): void {
+    this.imageChangedEvent = event;
+  }
+
+  imageCropped(event: ImageCroppedEvent) {
+    this.croppedImage = event.base64;
+    console.log(event, base64ToFile(event.base64));
+  }
+
+  imageLoaded() {
+    this.showCropper = true;
+    console.log('Image loaded');
+  }
+
+  cropperReady(sourceImageDimensions: Dimensions) {
+    console.log('Cropper ready', sourceImageDimensions);
+  }
+
+  loadImageFailed() {
+    console.log('Load failed');
+  }
+
+  toggleContainWithinAspectRatio() {
+    this.containWithinAspectRatio = !this.containWithinAspectRatio;
+  }
+
+  updateRotation() {
+    this.transform = {
+      ...this.transform,
+      rotate: this.rotation
+    };
+  }
+}
